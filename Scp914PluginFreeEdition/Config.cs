05fbca6afde9b0e62c9985a385ebd4b34e005be7
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-09-22 14:08:57 +0900

    test

--- Scp914PluginFreeEdition/Config.cs
+++ Scp914PluginFreeEdition/Config.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel;
 using Exiled.API.Interfaces;
 
 namespace Scp914PluginFreeEdition
 {
     public class Config : IConfig
     {
+        [Description("플러그인 활성화 여부")]
         public bool IsEnabled { get; set; } = true;
+        [Description("플러그인 디버그 여부")]
         public bool Debug { get; set; } = false;
-        public string ChangeRoleBroadcast { get; set; } = "당신은 %role 로 외형이 변화되었습니다.";
+
+        [Description("Veryfine 랜덤 효과")]
+        public string ChangeRoleBroadcast { get; set; } = "당신의 외형은 %name% 로 변경되었습니다.";
     }
 }
